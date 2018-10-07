60328f6c29030560dcad4e114f053ab0914c7c6e
Nick Brown <n.brown.professional@gmail.com>
2018-10-07 09:43:43 -0600

    adds in comment functionality to add and view comments for a summit

--- yelp-summit/app.js
+++ yelp-summit/app.js
@@ -1,8 +1,14 @@
 
 // npm packages
 var express = require("express"),
-    bodyParser = require("body-parser"),
-    mongoose = require("mongoose");
+	bodyParser = require("body-parser"),
+	mongoose = require("mongoose");
+	Summit = require("./models/summit"),
+	User = require("./models/user"),
+	Comment = require("./models/comment"),
+	SeedDB = require("./seeds")
+
+
 // initializing express
 var app = express();
 // connect mongoose
@@ -12,116 +18,108 @@ app.use(bodyParser.urlencoded({extended:true}));
 // sets .ejs files as our view engine so we don't have to state every time
 app.set("view engine","ejs");
 
-// =========== mongoose schema setup ===============
-
-var summitSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    spURL: String,
-    description: String
-
-})
-
-var Summit = mongoose.model("Summit", summitSchema)
-
-// =================================================
-
-// Summit.create({
-//             name: "A'Ghlas-Bheinn",
-//             image: "https://80d2853cc4def76b377d-54344bc01a8b066c84096a8e7a3499ac.ssl.cf1.rackcdn.com/medium/457959.jpg",
-//             spLink: "https://www.summitpost.org/a-ghlas-bheinn/605535",
-//             description: "A'Ghlas-Bheinn is a mountain reaching almost the minimum for a Munro, and a bit overshadowed by the neighbouring Beinn Fhada and Five Sisters of Kintail. However, if we combine its visit with the Falls of Glomach, it makes a very worth day-hike. The Falls of Glomach are UK 's (or Scotland's) third highest waterfall after Eas a ' Chual Aluinn in Assynt and Glen Nevis's Steall waterfall.But, by the verticality of the drop and the amount of water, they are probably the most spectacular, inspiring rather some Icelandic sights. A 'Ghlas-Bheinn is not less interesting, with rocky features, and great views around."
-//         }, function (err, res) {
-//     if (err) {
-//         console.log(Error);
-//     } else {
-//         console.log(res);
-        
-//     }
-// });
-
-//  test data - will delete when mongoDB is connected
-    // var summits = [{
-    //         name: "A'Ghlas-Bheinn",
-    //         image: "https://80d2853cc4def76b377d-54344bc01a8b066c84096a8e7a3499ac.ssl.cf1.rackcdn.com/medium/457959.jpg",
-    //         spLink: "https://www.summitpost.org/a-ghlas-bheinn/605535"
-    //     },
-    //     {
-    //         name: "A Muvrella",
-    //         image: "https://80d2853cc4def76b377d-54344bc01a8b066c84096a8e7a3499ac.ssl.cf1.rackcdn.com/medium/828983.jpg",
-    //         spLink: "https://www.summitpost.org/a-muvrella/828979"
-    //     },
-    //     {
-    //         name: "A2",
-    //         image: "https://80d2853cc4def76b377d-54344bc01a8b066c84096a8e7a3499ac.ssl.cf1.rackcdn.com/large/224799.jpg",
-    //         spLink: "https://www.summitpost.org/a2/224798"
-    //     }
-
-    // ]
-
 // port redirect for Heroku
 var PORT = process.env.PORT || 3000;
 
+// Uncomment to seed DB
+// SeedDB();
+
 // main route to render landing page
 app.get("/", function (req, res) {
-    res.render("landing")
+	res.render("landing")
 })
 
 // get route renders the summits page with all posted summits
 app.get("/summits", function (req, res) {
-    // get all summits from DB
-    Summit.find({}, function (err, summitResponse) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("index", {summits:summitResponse})
-        }
-    })
-    // res.render("summits", {summits:summits});
+	// get all summits from DB
+	Summit.find({}, function (err, summitResponse) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("summits/index", {summits:summitResponse})
+		}
+	})
+	// res.render("summits", {summits:summits});
 })
 
 // post route addss new summit to the summits array and redirects to summits get route
 app.post("/summits", function (req,res) {
-    // get data from form
-    var name = req.body.name;
-    var image = req.body.image;
-    var spLink = req.body.spLink;
-    var desc = req.body.description;
-    var newSummit = {name:name, image:image, spLink:spLink, description: desc}
-    // create new summit and add to DB
-    Summit.create(newSummit, function (err, newlyCreated) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(newlyCreated);
-            
-            // redirect to summits page
-            res.redirect("/summits");
-        }
-    })    
+	// get data from form
+	var name = req.body.name;
+	var image = req.body.image;
+	var spLink = req.body.spLink;
+	var desc = req.body.description;
+	var newSummit = {name:name, image:image, spLink:spLink, description: desc}
+	// create new summit and add to DB
+	Summit.create(newSummit, function (err, newlyCreated) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(newlyCreated);
 
-     
+			// redirect to summits page
+			res.redirect("/summits");
+		}
+	})
 })
 
 // renders the form that sends the new summit data
 app.get("/summits/new", function (req, res) {
-    res.render("new")    
+	res.render("summits/new")
 })
 
 // MUST BE AFTER /summits/new otherwise will treat/new as an id
 // GET route to show specific summit -- SHOW shows info about only one summit
 app.get("/summits/:id", function (req, res) {
-    //find the summit with provided ID
-    Summit.findById(req.params.id, function (err, foundSummit) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("show", {summit: foundSummit});
-        }
-    })
+	//find the summit with provided ID
+	// and populate the item with its associated comments
+	Summit.findById(req.params.id).populate("comments").exec(function (err, foundSummit) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("summits/show", {summit: foundSummit});
+		}
+	})
 });
 
 // shows the server is active and listening on the PORT being used
 app.listen(PORT, function () {
-    console.log("SummitPost Server Listening on PORT: " + PORT);
+	console.log("SummitPost Server Listening on PORT: " + PORT);
 });
+
+//==================================================
+// COMMENTS ROUTES
+//==================================================
+
+// gets all of the coments for a summit
+app.get("/summits/:id/comments/new", function(req, res){
+	// find summit by id
+	Summit.findById(req.params.id, function(err, summit){
+		if (err) {
+			console.log(err);
+		} else {
+			// send through
+			res.render("comments/new", {summit:summit});
+		}
+	})
+});
+
+app.post("/summits/:id/comments", function(req, res){
+	Summit.findById(req.params.id, function(err, summit) {
+		if (err) {
+			console.log(err);
+			res.redirect("/summits");
+		} else {
+			Comment.create(req.body.comment, function(err, comment){
+				if (err) {
+					console.log(err);
+				} else {
+					summit.comments.push(comment);
+					summit.save();
+					res.redirect('/summits/' + summit._id);
+				}
+			});
+		}
+	})
+
+})
